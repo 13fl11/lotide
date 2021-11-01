36a0b33f28c1ff0b9c56677e0a09861a5fe207ba
13fl11 <13fl11@queensu.ca>
2021-11-01 13:58:41 -0400

    refactor head

--- assertEqual.js
+++ assertEqual.js
@@ -10,5 +10,8 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+
+// module export
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,16 +1,24 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    let output1 = actual + ' === ' + expected;
-    console.log(`✅✅✅ Assertion Passed:  ${output1}`);
-  } else {
-    let output2 = actual + ' !== ' + expected;
-    console.log(`🛑🛑🛑 Assertion Failed:  ${output2}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     let output1 = actual + ' === ' + expected;
+//     console.log(`✅✅✅ Assertion Passed:  ${output1}`);
+//   } else {
+//     let output2 = actual + ' !== ' + expected;
+//     console.log(`🛑🛑🛑 Assertion Failed:  ${output2}`);
+//   }
+// };
+
+const assertEqual = require('./assertEqual');
 
 const head = function (actual) {
  return actual[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(head([5,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// TEST CODE
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+
+module.exports = head;
